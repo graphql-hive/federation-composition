2811dd1a5b58c14143ada3cd2e6f1510e8bf150a
Laurin Quast <laurinquast@googlemail.com>
2025-06-12 11:32:33 +0200

    no need to duplicate?

--- src/supergraph/validation/rules/satisfiablity/graph.ts
+++ src/supergraph/validation/rules/satisfiablity/graph.ts
@@ -355,12 +355,8 @@ export class Graph {
         );
       }
 
-      const newTail = this.duplicateNode(edge.tail);
-      const newEdge = new Edge(edge.head, edge.move, newTail);
-      this.replaceEdgeAt(edge.head.index, edge.tail.index, newEdge, edgeIndex);
-
       queue.push({
-        head: newTail,
+        head: edge.tail,
         providedFields,
       });
     }
@@ -450,12 +446,8 @@ export class Graph {
         );
       }
 
-      const newTail = this.duplicateNode(edge.tail);
-      const newEdge = new Edge(edge.head, edge.move, newTail);
-      this.replaceEdgeAt(edge.head.index, edge.tail.index, newEdge, edgeIndex);
-
       queue.push({
-        head: newTail,
+        head: edge.tail,
         providedFields,
       });
     }
@@ -668,16 +660,12 @@ export class Graph {
 
         // find field edges that are provided and mark them as resolvable.
         // if they are not available, create them
-        const newTail = this.duplicateNode(edge.tail);
-        const newEdge = new Edge(edge.head, edge.move, newTail);
-        this.replaceEdgeAt(headIndex, edge.tail.index, newEdge, edgeIndex);
-
         const queue: {
           head: Node;
           providedFields: SelectionNode[];
         }[] = [
           {
-            head: newTail,
+            head: edge.tail,
             providedFields: edge.move.provides.selectionSet,
           },
         ];
